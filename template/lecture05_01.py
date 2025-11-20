4bb1030229f44c48f2198432f3f807669bb6f8ba
MONSTER0129 <haruku0129@gmail.com>
2025-11-20 13:59:06 +0900

    Update lecture05_01.py

--- template/lecture05_01.py
+++ template/lecture05_01.py
@@ -9,7 +9,7 @@ def lecture05_01():
     capture_img = app.get_img()
 
     # 画像を読み込み
-    google_img = cv2.imread('/Users/k24027kk/work/oop2/05/issue/images/google.png')
+    google_img = cv2.imread('template/images/google.png')
 
     if google_img is None:
         print("google.png が読み込めませんでした。")
